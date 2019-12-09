e636707979bcc8a9d28fb9f8ab5db6955c6b01b5
bext <jalbertomr@yahoo.com>
2019-12-09 14:08:05 -0600

    concatWith( Flux.just("...")...concatWith( Flux.just().. If Exception flux stopped

--- src/test/java/com/bext/fluxmonoroad/FluxMonoTest.java
+++ src/test/java/com/bext/fluxmonoroad/FluxMonoTest.java
@@ -8,7 +8,9 @@ class FluxMonoTest {
     @Test
     void fluxTest() {
         Flux<String> fluxString = Flux.just("Elemento1", "Elemento2", "Elemento3")
+                .concatWith( Flux.just("Esto se Mostrara"))
                 .concatWith( Flux.error( new RuntimeException("Exception generated voluntarily")))
+                .concatWith( Flux.just("Esto NO se Mostara"))
                 .log();
         fluxString.subscribe(System.out::println, ex ->System.err.println("El error es :" + ex));
     }
