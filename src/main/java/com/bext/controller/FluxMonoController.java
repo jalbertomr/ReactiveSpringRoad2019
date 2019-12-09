018aa73087979180aea455ca69b549665258da31
bext <jalbertomr@yahoo.com>
2019-12-09 17:18:46 -0600

    WebFlux Non Blocking API .delayElements(Duration.ofSeconds(1))

--- src/main/java/com/bext/controller/FluxMonoController.java
+++ src/main/java/com/bext/controller/FluxMonoController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 @RestController
 public class FluxMonoController {
     @GetMapping("/flux")
     Flux<Integer> flux() {
         return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
                 .log();
     }
 }
