63236666d487ff4b0c8454f34d984ec1c9c35cad
bext <jalbertomr@yahoo.com>
2019-12-09 15:57:17 -0600

    JUnit Test .expectNext("ElementX") ... .verifyComplete()

--- src/test/java/com/bext/fluxmonoroad/FluxMonoTest.java
+++ src/test/java/com/bext/fluxmonoroad/FluxMonoTest.java
@@ -2,6 +2,7 @@ package com.bext.fluxmonoroad;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class FluxMonoTest {
 
@@ -15,5 +16,16 @@ class FluxMonoTest {
         fluxString.subscribe(System.out::println,
                              ex -> System.err.println("El error es :" + ex),
                              () -> System.out.println("Completado"));
+    }
+
+    @Test
+    void fluxElements_WithoutError(){
+        Flux<String> fluxString = Flux.just("Elemento1","Elemento2","Elemento3")
+                .log();
+        StepVerifier.create( fluxString)
+                .expectNext("Elemento1")
+                .expectNext("Elemento2")
+                .expectNext("Elemento3")
+                .verifyComplete();
     }
 }
