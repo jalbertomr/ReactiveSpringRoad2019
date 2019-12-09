89ac80d9e3ecc6dbc736d1a533ee302c2b007972
bext <jalbertomr@yahoo.com>
2019-12-09 13:52:06 -0600

    first commit Flux suscribe(Consumer <? super String> consumer,Consumer <? super Throwable> errorConsumer) e -> System.err.println(ex)

--- src/test/java/com/bext/fluxmonoroad/FluxMonoTest.java
+++ src/test/java/com/bext/fluxmonoroad/FluxMonoTest.java
@@ -7,7 +7,8 @@ class FluxMonoTest {
 
     @Test
     void fluxTest() {
-        Flux<String> fluxString = Flux.just("Elemento1", "Elemento2", "Elemento3");
-        fluxString.subscribe(System.out::println);
+        Flux<String> fluxString = Flux.just("Elemento1", "Elemento2", "Elemento3")
+                .concatWith( Flux.error( new RuntimeException("Exception generated voluntarily")));
+        fluxString.subscribe(System.out::println, ex -> System.err.println(ex));
     }
 }
