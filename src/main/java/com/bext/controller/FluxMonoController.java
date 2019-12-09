3b1c701c574112fc82787ad5101f9b22cf2dafce
bext <jalbertomr@yahoo.com>
2019-12-09 17:30:33 -0600

    WebFlux Non Blocking API .delayElements(Duration.ofSeconds(1)) MediaType.APPLICATION_STREAM_JSON_VALUE

--- src/main/java/com/bext/controller/FluxMonoController.java
+++ src/main/java/com/bext/controller/FluxMonoController.java
@@ -1,17 +1,27 @@
 package com.bext.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.awt.*;
 import java.time.Duration;
 
 @RestController
 public class FluxMonoController {
     @GetMapping("/flux")
-    Flux<Integer> flux() {
+    public Flux<Integer> flux() {
         return Flux.just(1,2,3,4)
                 .delayElements(Duration.ofSeconds(1))
                 .log();
     }
+
+    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> fluxStream() {
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
 }
